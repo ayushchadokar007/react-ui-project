ec4de72139685453dad327ace0529c4e95a0f906
Ayush Chadokar <153833960+ayushchadokar007@users.noreply.github.com>
2026-03-02 23:00:24 +0530

    Add files via upload

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <button id="btn1">Target Audience</button>
+      <button id='btn2'>Digital Profile Platform</button>
+    </div>
+  )
+}
+
+export default Navbar
